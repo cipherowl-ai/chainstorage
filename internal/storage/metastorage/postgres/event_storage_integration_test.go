1059c8da3e47eacd7458e8dcb1cfff87b6fbe5ee
William <williamchenn123@gmail.com>
2025-06-22 15:31:17 +0800

    get rid of transaction table drop in tests

--- internal/storage/metastorage/postgres/event_storage_integration_test.go
+++ internal/storage/metastorage/postgres/event_storage_integration_test.go
@@ -57,7 +57,7 @@ func (s *eventStorageTestSuite) TearDownTest() {
 		ctx := context.Background()
 		s.T().Log("Clearing database tables after test")
 		// Clear all tables in reverse order due to foreign key constraints
-		tables := []string{"transactions", "block_events", "canonical_blocks", "block_metadata"}
+		tables := []string{"block_events", "canonical_blocks", "block_metadata"}
 		for _, table := range tables {
 			_, err := s.db.ExecContext(ctx, fmt.Sprintf("DELETE FROM %s", table))
 			if err != nil {
