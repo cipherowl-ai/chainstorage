7d77c3d4a57491139c666d94ad4bb9f8becdeba9
b-x-cipherowl <boyang.xu@cipherowl.com>
2026-07-21 17:09:45 +0800

    fix(INF-1126): persist empty delete marker arrays

--- internal/storage/retirement/postgres_repository.go
+++ internal/storage/retirement/postgres_repository.go
@@ -238,6 +238,10 @@ func (r *PostgresRepository) PrepareRetirement(ctx context.Context, manifest Ret
 		VALUES ($1, $2, $3, NULLIF($4, ''), $5, $6, $7, $8, $9, $10, $11,
 			$12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $21)
 		ON CONFLICT (block_metadata_id) DO NOTHING`
+	deleteMarkerVersionIDs := manifest.SingleBlockDeleteMarkerVersionIDs
+	if deleteMarkerVersionIDs == nil {
+		deleteMarkerVersionIDs = []string{}
+	}
 	if _, err := tx.ExecContext(ctx, insert,
 		manifest.BlockMetadataID,
 		manifest.Tag,
@@ -248,7 +252,7 @@ func (r *PostgresRepository) PrepareRetirement(ctx context.Context, manifest Ret
 		manifest.SingleBlockObjectKey,
 		manifest.SingleBlockObjectKeySHA256,
 		pq.Array(manifest.SingleBlockObjectVersionIDs),
-		pq.Array(manifest.SingleBlockDeleteMarkerVersionIDs),
+		pq.Array(deleteMarkerVersionIDs),
 		manifest.SingleBlockObjectETag,
 		manifest.SingleBlockObjectBytes,
 		manifest.ConsolidatedObjectKey,
